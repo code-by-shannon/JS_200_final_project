fa076ad792cbb33a2806da55a3dba722df2fd99f
Shannon <sestumpf@gmail.com>
2025-12-08 21:41:24 -0800

    add css

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
 const formEl = document.querySelector('form');
-const taskEl = document.querySelector('#tasks');
+const tasksEl = document.querySelector('#tasks');
 
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+console.log(tasks);
+
+tasksEl.addEventListener('click', (e)=>{
+    const action = e.target.getAttribute('data-action');
+    const index = e.target.getAttribute('data-index');
+    console.log(action, index);
+
+    if(action === 'delete'){
+        tasks.splice(index, 1);
+        saveAndRenderTasks(tasks);
+    }
+});
+
+const saveAndRenderTasks = (tasks) => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    renderTasks(tasks);
+}
 
 formEl.addEventListener('submit', (e)=>{
     e.preventDefault();
     const value = formEl.elements['task'].value;
-    console.log(value);
+    
+    tasks.push({title: value});
+    formEl.reset();
+    saveAndRenderTasks(tasks);
+
 })
+
+const renderTasks = (tasks) => {
+    const tasksHTML = tasks.map((task, index) => {
+        return `<li>${task.title} <button data-action = 'delete' data-index = ${index}>delete</button></li>`;
+    }).join("");
+
+    tasksEl.innerHTML = tasksHTML;
+};
+
+renderTasks(tasks);
